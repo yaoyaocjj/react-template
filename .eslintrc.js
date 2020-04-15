b09496ad7fe930f9f7c003568b93e8d58d682829
Chen Juanjuan <juanjuan.chen@rea-group.com>
2020-04-15 17:45:43 +0800

    Init eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-  },
-  'extends': [
-    'plugin:react/recommended'
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+    "env": {
+        "browser": true,
+        "es6": true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'settings': {
-    'version': '^16.13.1'
-  },
-  'rules': {
-  }
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
