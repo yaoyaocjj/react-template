3c563c7d04b562dad2e70b847ff8101d3e9f1fa2
yaoyaocjj <1506622086@qq.com>
2020-04-16 09:15:47 +0800

    Test git

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 
-const template = <p>Hello from react</p>;
+const template = <p>
+  Hello from react
+  Test git
+</p>;
 
 ReactDOM.render(template, document.getElementById('root'));
