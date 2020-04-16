cf6337e5cd506ce602c00ac0747ef8364534ee0d
yaoyaocjj <1506622086@qq.com>
2020-04-16 09:17:53 +0800

    Reset

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 
-const template = <p>
-  Hello from react
-  Test git
-</p>;
+const template = <p>Hello from react</p>;
 
 ReactDOM.render(template, document.getElementById('root'));
